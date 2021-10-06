4ccbc3ab104b16750127d72f7c0688e1553cc767
Balázs Heves <balazs.heves@stud.uni-corvinus.hu>
2021-10-06 22:38:53 +0200

    Data exported into the excel sheet

--- data_export/data_export/Form1.cs
+++ data_export/data_export/Form1.cs
@@ -23,9 +23,9 @@ namespace data_export
 
         public Form1()
         {
-            InitializeComponent();
+            //InitializeComponent();
             LoadData();
-           // CreateExcel();
+            CreateExcel();
         }
 
         private void CreateExcel()
@@ -63,7 +63,79 @@ namespace data_export
 
         private void CreateTable()
         {
-            
+            //az excel header neveinek megadása
+            string[] headers = new string[] {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"};
+            // A header kiírása az excel első sorába
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            // Objektum feltöltése a lakások adataival
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            string[] kepletek = new string[values.GetLength(0)];
+
+            for (int i = 0; i < values.GetLength(0); i++)
+            {
+                kepletek[i] = string.Format("=G{0}*H{0}",i+2);
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 9),
+            GetCell(1 + values.GetLength(0), 9)).Value2 = kepletek[0];
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
         private void LoadData()
