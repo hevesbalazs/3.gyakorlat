147f723ae775bda9ab7b8aa5cc5a25b75895d1f9
Balázs Heves <balazs.heves@stud.uni-corvinus.hu>
2021-10-26 17:21:20 +0200

    WebService Called

--- ArfolyamLekerdezes/ArfolyamLekerdezes/Form1.cs
+++ ArfolyamLekerdezes/ArfolyamLekerdezes/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ArfolyamLekerdezes.MnbServiceReference;
 
 namespace ArfolyamLekerdezes
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            CallWebservice();
+        }
+
+        private void CallWebservice()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;           
         }
     }
 }
