80706cc95ff71957850172bcccb9b95f436395f7
Balázs Heves <balazs.heves@stud.uni-corvinus.hu>
2021-11-23 22:29:14 +0100

    Password validation tested

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,15 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
+            // Arrange
+            var accountController = new AccountController();
 
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [
+            Test,
+            TestCase("Abcdefgh", false),
+            TestCase("ABCDEFG1", false),
+            TestCase("abcdefg1", false),
+            TestCase("Abcd1", false),
+            TestCase("Abcdefg1", true)           
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
