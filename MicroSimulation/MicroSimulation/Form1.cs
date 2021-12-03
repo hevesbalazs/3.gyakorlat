dfce4182e15f9bd074440897f722832a1fdd765d
Balázs Heves <balazs.heves@stud.uni-corvinus.hu>
2021-12-03 21:54:35 +0100

    Simulation Step Created

--- MicroSimulation/MicroSimulation/Form1.cs
+++ MicroSimulation/MicroSimulation/Form1.cs
@@ -29,7 +29,7 @@ namespace MicroSimulation
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-
+                    SimStep(year, Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -102,6 +102,35 @@ namespace MicroSimulation
                 }
             }
             return DeathProbabilities;
+        }
+
+        public void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.ChanceofDeath).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath) person.IsAlive = false;
+
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.ChanceofBirth).FirstOrDefault();
+            
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
